f8f5bf86d0f06874e66aeb911da9b1855a74099b
Chandler Scott <scottcd1@etsu.edu>
2023-06-05 17:56:49 -0400

    repo created

---
+++ README.md
@@ -0,0 +1,8 @@
+### to install dependencies
+python3 setup.py install --user
+
+### to install package in python env
+pip install .
+
+### to import in code
+from ai_algorithms import *
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='artificial_intelligence',
+    version='1.0.0',
+    description='Three AI algorithms',
+    author='Chandler Scott',
+    author_email='scottcd1@etsu.edu',
+    packages=find_packages(),
+    install_requires=[
+        'torch>=1.8.0',
+        'torchvision>=0.9.0',
+        # Add other dependencies if needed
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
